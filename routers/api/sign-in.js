c4a2f5ae4ed6b199f57b37a04b56079b8f71a516
maizhiying <zhiying.mai@gmail.com>
2016-06-02 15:52:27 +0800

    完善登入接口

--- routers/api/sign-in.js
+++ routers/api/sign-in.js
@@ -5,21 +5,45 @@ var User = require('../../models/user')
 var objectId = require('mongoose').Types.ObjectId
 var md5 = require('md5')
 
+var dummyUser = {
+  name: '',
+  id: ''
+}
+
 router
   .route('/')
   .post(function (req, res) {
     if (req.session.user) {
-      res.send('already sign in, user.name: ' + req.session.user.name)
+      res.send({
+        success: false,
+        message: '已经登入了',
+        user: dummyUser
+      })
     } else {
       User.findOne({ name: req.body.name, password: md5(req.body.password)}).then(function (user) {
         if (user) {
           req.session.user = user
-          res.send(user)
+          res.send({
+            success: true,
+            message: '登入成功',
+            user: {
+              id: user._id,
+              name: user.name
+            }
+          })
         } else {
-          res.send('invalid name or password')
+          res.send({
+            success: false,
+            message: '用户名或者密码不符',
+            user: dummyUser
+          })
         }
       }, function (err) {
-        res.send(err)
+        res.send({
+          success: false,
+          message: String(err),
+          user: dummyUser
+        })
       })
     }
   })
