f3ad50a67efd5792ddc3995803fc5e7d67a7e58b
maizhiying <zhiying.mai@gmail.com>
2016-06-03 15:09:12 +0800

    完善创建用户接口

--- routers/api/sign-up.js
+++ routers/api/sign-up.js
@@ -5,20 +5,33 @@ var User = require('../../models/user')
 var objectId = require('mongoose').Types.ObjectId
 var md5 = require('md5')
 
+var messages = {
+  '11000': '用户名已存在'
+}
+
 router
   .route('/')
   .post(function (req, res) { 
     if (req.session.user) {
-      res.send('already sign in')
+      res.send({
+        success: false,
+        message: '已经登入了'
+      })
     } else {
       User.create({
         _id: objectId(),
         name: req.body.name.trim(),
         password: md5(req.body.password)
       }).then(function () {
-        res.send('create user')
+        res.send({
+          success: true,
+          message: '创建成功'
+        })
       }, function (err) {
-        res.send(err)
+        res.send({
+          success: false,
+          message: messages[String(err.code)]
+        })
       })
     }
   })
